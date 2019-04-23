a8e63c618427370eaf3071b0933a7f39223bc863
许广华 <fortnight@xuguanghuadeMBP.lan>
2019-04-24 00:40:54 +0800

    ～2019-04-23

--- app/main.js
+++ app/main.js
@@ -1,1 +1,41 @@
-console.log('This is an Electron application!');
+const fs = require('fs');
+
+const { app, BrowserWindow, dialog } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({ show: false });
+
+    mainWindow.loadFile(`${__dirname}/index.html`);
+
+    // getFileFromUser();
+
+    mainWindow.once('ready-to-show', () => mainWindow.show());
+})
+
+exports.getFileFromUser = () => {
+    const files = dialog.showOpenDialog({
+        properties: ['openFile'],
+        buttonLabel: '打开',
+        filters: [
+            { name: 'Markdown File', extensions: ['md', 'mdown', 'markdown', 'marcdown']}
+        ]
+    });
+
+    if (!files) return;
+
+    const file = files[0];
+    
+    openFile(file);
+}
+
+const openFile = (file) => {
+    const content = fs.readFileSync(file).toString();
+
+    // 添加打开过的文件到历史记录里
+    app.addRecentDocument(file);
+    // 向渲染进程发送文件打开事件
+    mainWindow.webContents.send('file-opened', file, content);
+}
+
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,21 @@
+const path = require('path');
 const marked = require('marked');
+const { remote, ipcRenderer } = require('electron');
+const mainProcess = remote.require('./main.js');
+const currentWindow = remote.getCurrentWindow();
+
+const CONST_VARIABLES = {
+  TITLE: 'mars editor'
+};
+
+const APP_STATES = {
+  title: CONST_VARIABLES.TITLE,
+  isEdited: false,
+  originalContent: '',
+  file: null
+};
+
+
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -14,7 +31,56 @@ const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateInterface = (currWindow) => {
+  currWindow.setTitle(APP_STATES.title);
+  // 按钮可用性修改
+  saveMarkdownButton.disabled = !APP_STATES.isEdited;
+  revertButton.disabled = !APP_STATES.isEdited;
+
+  currWindow.setRepresentedFilename(APP_STATES.file);
+  currWindow.setDocumentEdited(APP_STATES.isEdited);
+};
+
+const updateStates = action => {
+  const { key, value } = action;
+  if (typeof key === 'string') return APP_STATES[key] = value;
+
+  if (key.slice && key.forEach) {
+    return key.forEach((keyItem, index) => APP_STATES[keyItem] = value[index]);
+  }
+};
+
+const editedTitleCal = isEdited => {
+  if (!isEdited) return APP_STATES.title.replace('  ●', '');
+  if (APP_STATES.title.includes('●')) return APP_STATES.title;
+  return `${APP_STATES.title}  ●`;
+};
+
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
+  const isEdited = currentContent !== APP_STATES.originalContent;
+
   renderMarkdownToHtml(currentContent);
+  updateStates({
+    key: ['isEdited', 'title'],
+    value: [isEdited, editedTitleCal(isEdited)]
+  });
+  updateInterface(currentWindow);
 });
+
+
+openFileButton.addEventListener('click', event => {
+  mainProcess.getFileFromUser();
+})
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+
+  const title =`${path.basename(file)} - ${CONST_VARIABLES.TITLE}`;
+  updateStates({
+    key: ['originalContent', 'title', 'file'],
+    value: [content, title, file]
+  });
+  updateInterface(currentWindow);
+})
